3f274bb27a45a16ef9f279b0beeb86acee3bb26c
coringuyen <coringuyen96@gmail.com>
2015-12-01 21:26:13 -0600

    Start working on it

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+
+    public Vector3 Pos;
+    public Vector3 Vel;
+    Vector3 momentum;
+    Rigidbody rb;
+    public Vector3 force;
+    public Vector3 acceleration;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        Pos = transform.position;
+        Vel = rb.velocity;
+	}
+
+    void FixedUpdate()
+    {
+        acceleration = force / rb.mass;
+        momentum = rb.mass * Vel;
+
+        // Euler Intergration
+        Vel = Vel + acceleration * Time.deltaTime;
+        rb.velocity = Vel;
+
+        Pos = Pos + Vel * Time.deltaTime;
+        transform.position = Pos;
+    }
+}
---
+++ Assets/Scripts/SpringDamper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringDamper : MonoBehaviour {
+
+    float SpringContants;
+    float DampingFactor;
+    float RestLength;
+
+    Particle p1;
+    Particle p2;
+
+    public void ComputeForce()
+    {
+        
+    }
+	
+}

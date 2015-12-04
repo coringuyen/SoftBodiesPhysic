572745c340a57a7367f5fdfb8bbb4e3b1e89c735
coringuyen <coringuyen96@gmail.com>
2015-12-04 00:59:57 -0600

    making grid

--- Assets/Scripts/ClothSimulation.cs
+++ Assets/Scripts/ClothSimulation.cs
@@ -10,14 +10,27 @@ public class ClothSimulation : MonoBehaviour
 
 	void Start()
 	{
-		for (int i = 0; i < 20; ++i) 
-		{
-			GameObject particle = Instantiate(Particles) as GameObject;
-			particle.transform.position = new Vector3();
-			particles.Add(particle);
-		}
+		for (int i = 0; i < 5; ++i) 
+            for(int j = 0; j < 5; ++j)
+		    {
+			    GameObject particle = Instantiate(Particles) as GameObject;
+			    particle.transform.position = new Vector3(i * 20 / 5, j * 20 / 5, 0);
+			    particles.Add(particle);
+		    }
+    
 	}
 
+    void Update()
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            Debug.DrawLine(new Vector3(i, 0f, 0f), new Vector3(i + 5f, 0f, 0f), Color.red);
+            for (int j = 0; j < 5; ++j)
+            {
+                Debug.DrawLine(new Vector3(0f, j, 0f), new Vector3(0, j + 1, 0f), Color.red);
+            }
+        }
+    }
 	void FixedUpdate()
 	{
 		foreach (GameObject o in particles) 
